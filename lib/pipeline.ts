0904bb288cef559f67513c538c48bf1079025a80
Roman <rlozanoc@amazon.com>
2022-11-04 23:22:41 +0000

    Change EKS cluster instance type

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -4,6 +4,9 @@ import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import { KubecostAddOn } from '@kubecost/kubecost-eks-blueprints-addon';
 import { TeamPlatform, TeamApplication } from '../teams'; // HERE WE IMPORT TEAMS
+import { InstanceType } from 'aws-cdk-lib/aws-ec2';
+import { CapacityType, KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+import { GenericClusterProvider } from '@aws-quickstart/eks-blueprints';
 
 
 export default class PipelineConstruct extends Construct {
@@ -16,12 +19,25 @@ export default class PipelineConstruct extends Construct {
     const addOnKubecost = new KubecostAddOn();
     const addOnAWSLBC = new blueprints.AwsLoadBalancerControllerAddOn();
 
+    // Managed Node Group
+    const clusterProvider = new GenericClusterProvider({
+      version: KubernetesVersion.V1_21,
+      managedNodeGroups: [{
+        id: `${id}-nodegroup`,
+        instanceTypes: [new InstanceType('t4g.small')],
+        minSize: 1,
+        maxSize: 10,
+        desiredSize: 4
+      }],
+    })
+    
     const blueprint = blueprints.EksBlueprint.builder()
     .account(account)
     .region(region)
     .addOns(new blueprints.ClusterAutoScalerAddOn,
             addOnKubecost,
             addOnAWSLBC)
+    .clusterProvider(clusterProvider)
     .teams(new TeamPlatform(account), new TeamApplication('burnham',account)); // HERE WE ADD THE TEAMS
   
     blueprints.CodePipelineStack.builder()
